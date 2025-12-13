73a662e4d724f556f0f1b179b88a3670410f8329
author <author@computer>
2025-12-13 22:49:02 +0000

    new readme file

--- deployment/deploy-config.sh
+++ deployment/deploy-config.sh
@@ -4,7 +4,7 @@
 # Build target: "local" for Mac testing, "aws" for AWS deployment
 # - local: builds for your Mac's architecture (arm64 on Apple Silicon)
 # - aws: builds for linux/amd64 (required for AWS Fargate)
-BUILD_TARGET="aws"
+BUILD_TARGET= "local" #"aws"
 
 # Set Docker platform based on build target
 if [ "$BUILD_TARGET" = "aws" ]; then
